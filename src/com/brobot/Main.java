973bb02d815507286bb854735b98d4279a19f291
Nathan M <39346843+nathanmeira@users.noreply.github.com>
2021-07-07 11:32:31 -0300

    Update Main.java

--- src/com/brobot/Main.java
+++ src/com/brobot/Main.java
@@ -5,28 +5,27 @@ public class Main {
     public static void main(String[] args) {
 
         /**
-         * Todos os testes devem sair o resultado no console igual ao exemplo a baixo
-         * Examples:
-         * System.out.println(funcaoPronta()) // "Ok"
+         * Todos os exercícios devem printar o resultado no console igual ao exemplo abaixo:
          */
 
-        System.out.println(funcaoPronta());
+        System.out.println(funcaoPronta()); // "Ok"
 
         /**
-        * 1- Capitalize
+        * 1 - Capitalize
         *
         * Write a function that accepts a string. The function should
         * capitalize the first letter of each word in the string then
         * return the capitalized string.
         *
         * Examples:
-        * System.out.println(capitalize("a short sentence")) // "A Short Sentence"
-        * System.out.println(capitalize("a lazy fox")) // "A Lazy Fox"
-        * System.out.println(capitalize("look, it is working!")) // "Look, It Is Working!"
         */
+        
+        // System.out.println(capitalize("a short sentence")) // "A Short Sentence"
+        // System.out.println(capitalize("a lazy fox")) // "A Lazy Fox"
+        // System.out.println(capitalize("look, it is working!")) // "Look, It Is Working!"
 
         /**
-        * 2- Longest Word
+        * 2 - Longest Word
         *
         * Write a function that returns the longest word in the passed sentence.
         * If there are two or more words that are the same length, return
@@ -34,44 +33,52 @@ public class Main {
         * and assume sentence will not be empty.
         *
         * Examples:
-        * System.out.println(longestWord("Hello there")) // "Hello"
-        * System.out.println(longestWord("My name is Adam")) // "name"
-        * System.out.println(longestWord("fun&!! time")) // "time"
         */
+        
+        // System.out.println(longestWord("Hello there")) // "Hello"
+        // System.out.println(longestWord("My name is Adam")) // "name"
+        // System.out.println(longestWord("fun&!! time")) // "time"
 
 
         /**
-        * 3 - Dado uma string e uma letra deve retornar a primeira posição da letra
+        * 3 - Dado uma string e uma letra, deve-se retornar a posição da primeira incidência desta letra
         *
-        * Exemple
-        * System.out.println(acharPosicao("Lambada", "a")) // 1
-        * System.out.println(acharPosicao("Lambada", "d")) // 5
-        * System.out.println(acharPosicao("fusca", "f")) // 0
-        * System.out.println(acharPosicao("aAaA", "A")) // 1
+        * Exemplo
         */
+        
+        // System.out.println(acharPosicao("Lambada", "a")) // 1
+        // System.out.println(acharPosicao("Lambada", "d")) // 5
+        // System.out.println(acharPosicao("fusca", "f")) // 0
+        // System.out.println(acharPosicao("aAaA", "A")) // 1
 
         /**
-        * 4 - Dado dois núremos deve comparar o primeiro número com o segundo.
+        * 4 - Dado dois números, deve-se comparar o primeiro número com o segundo.
         *
-        * Exemple
-        * System.out.println(quemEhMaior(10, 1)) // "Número [10] é maior do que número [1]"
-        * System.out.println(quemEhMaior(-10, 1)) // "Número [-10] é menor do que número [1]"
-        * System.out.println(quemEhMaior(0, 0)) // "Empate!"
+        * Exemplo
         */
+        
+        // System.out.println(quemEhMaior(10, 1)) // "Número [10] é maior do que número [1]"
+        // System.out.println(quemEhMaior(-10, 1)) // "Número [-10] é menor do que número [1]"
+        // System.out.println(quemEhMaior(0, 0)) // "Empate!"
 
         /**
          *  5 - Exercícios com data
-         *  5.1 - Pegar hora do Systema e retornar nos seguintes formatos:
+         *
+         *  5.1 - Pegar hora do sistema e retornar nos seguintes formatos:
          *  "YYYY-MM-DD HH:mm:ss"
          *  "DD/MM/YYYY"
+         *
          *  5.2 - Pegar o primeiro dia e o último dia do mês corrente
          */
 
         /**
-         * 6 - Exercícios programação orientado a objeto
-         * 6.1 - Cria as class de acordo com o diagrama na imagem "diagrama-de-classe.png"
-         * Instanciar um objeto da classe Pessoa, preecher os atributos e chamar os métodos
-         * 6.2 - Instanciar dois objetos da classe Cachorro e associar ao objeto da classe Pessoa
+         * 6 - Exercícios de programação orientada a objeto
+         *
+         * Instruções: 
+         *
+         * 1) Criar as classes de acordo com o diagrama na imagem "diagrama-de-classes.png".
+         * 2) Instanciar um objeto da classe Pessoa, preechendo os atributos e chamando os métodos.
+         * 3) Instanciar dois objetos da classe Cachorro e associar ao objeto da classe Pessoa.
          */
     }
 
